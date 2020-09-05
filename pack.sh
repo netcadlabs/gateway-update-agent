dd7b3a65067f7b025b98819b2502532b5786827d
Koray Orhun <korhun@gmail.com>
2020-09-05 20:00:23 +0300

    NDU-262

--- pack.sh
+++ pack.sh
@@ -4,8 +4,8 @@ clear
 rm -rf bin obj pack
 dotnet restore
 dotnet deb -c Release -r linux-x64
-# dotnet deb -c Release -r linux-arm
-# dotnet deb -c Release -r linux-arm64
+dotnet deb -c Release -r linux-arm
+dotnet deb -c Release -r linux-arm64
 
 mkdir ./pack/
 cp ./bin/Release/netcoreapp3.1/linux-x64/GatewayUpdateAgent*linux-x64.deb ./pack/ 2>/dev/null
